94f1764d4b6855a9075baa4a376c08f33d9a04ab
fabraz <fabraz@fabraz>
2014-03-11 20:11:07 -0300

    extract method

--- src/br/unb/fga/das/Customer.java
+++ src/br/unb/fga/das/Customer.java
@@ -27,25 +27,10 @@ class Customer {
 		while (rentals.hasMoreElements()) {
 			double thisAmount = 0;
 			Rental each = (Rental) rentals.nextElement();
+			
+			thisAmount = amountFor(each);
 
-			// determine amounts for each line
-			switch (each.getMovie().getPriceCode()) {
-			case Movie.REGULAR:
-				thisAmount += 2;
-				if (each.getDaysRented() > 2)
-					thisAmount += (each.getDaysRented() - 2) * 1.5;
-				break;
-			case Movie.NEW_RELEASE:
-				thisAmount += each.getDaysRented() * 3;
-				break;
-			case Movie.CHILDRENS:
-				thisAmount += 1.5;
-
-				if (each.getDaysRented() > 3)
-					thisAmount += (each.getDaysRented() - 3) * 1.5;
-				break;
-
-			}
+			
 			// add frequent renter points
 			frequentRenterPoints++;
 			// add bonus for a two day new release
@@ -69,5 +54,27 @@ class Customer {
 				+ "frequent renter points";
 
 		return result;
+	}
+
+	private double amountFor(Rental each) {
+		int thisAmount = 0;
+		// determine amounts for each line
+		switch (each.getMovie().getPriceCode()) {
+		case Movie.REGULAR:
+			thisAmount += 2;
+			if (each.getDaysRented() > 2)
+				thisAmount += (each.getDaysRented() - 2) * 1.5;
+			break;
+		case Movie.NEW_RELEASE:
+			thisAmount += each.getDaysRented() * 3;
+			break;
+		case Movie.CHILDRENS:
+			thisAmount += 1.5;
+
+			if (each.getDaysRented() > 3)
+				thisAmount += (each.getDaysRented() - 3) * 1.5;
+			break;
+		}		
+		return thisAmount;
 	}
 }
