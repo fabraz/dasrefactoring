c0ba7d56d1ddc8a141fc87a5be8a98aeca168ff8
fabraz <fabricio.braz@gmail.com>
2014-03-13 22:23:39 -0300

    general updates

--- src/br/unb/fga/das/Customer.java
+++ src/br/unb/fga/das/Customer.java
@@ -28,7 +28,7 @@ class Customer {
 			double thisAmount = 0;
 			Rental each = (Rental) rentals.nextElement();
 			
-			thisAmount = amountFor(each);
+			thisAmount = each.getCharge();
 
 			
 			// add frequent renter points
@@ -57,24 +57,6 @@ class Customer {
 	}
 
 	private double amountFor(Rental aRental) {
-		int result = 0;
-		// determine amounts for each line
-		switch (aRental.getMovie().getPriceCode()) {
-		case Movie.REGULAR:
-			result += 2;
-			if (aRental.getDaysRented() > 2)
-				result += (aRental.getDaysRented() - 2) * 1.5;
-			break;
-		case Movie.NEW_RELEASE:
-			result += aRental.getDaysRented() * 3;
-			break;
-		case Movie.CHILDRENS:
-			result += 1.5;
-
-			if (aRental.getDaysRented() > 3)
-				result += (aRental.getDaysRented() - 3) * 1.5;
-			break;
-		}		
-		return result;
+		return aRental.getCharge();
 	}
 }
